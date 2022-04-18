f9df5d4d63c1a1b9b9f46419dc9991ae6f52e78a
Sardinka26 <sardinka2021@gmail.com>
2022-04-18 23:33:42 +0300

    AutoNumberPart2

--- src/Module_10/CoolNumbers_Option_2.java
+++ src/Module_10/CoolNumbers_Option_2.java
@@ -1,27 +1,22 @@
 package Module_10;
+
 import java.util.*;
 
 public class CoolNumbers_Option_2 {
-    private static final String[] letters = new String[]{"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
-    private static final String ANSWER_SYSTEM_TIME = "Поиск занял: ";
-    private static final String ANSWER_SYSTEM_SEARCH_ARRAY_LIST = "Поиск перебором: ";
-    private static final String ANSWER_SYSTEM_ERROR = "Номер НЕ найден: ";
-    private static final String ANSWER_SYSTEM = "Номер найден! ";
-    private static final String ANSWER_SYSTEM_SEARCH_BINARY = "Бинарный поиск : ";
-    private static final String ANSWER_SYSTEM_SEARCH_HASH_SET = "Поиск в HashSet: ";
-    private static final String ANSWER_SYSTEM_SEARCH_TREE_SET = "Поиск в TreeSet: ";
+    private static final String[] LETTERS = new String[]{"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
+
 
     public static List<String> generateCoolNumbers() {
         ArrayList<String> listNumber = new ArrayList<>();
 
-        for (int i = 0; i < letters.length; i++) {
-            for (int l = 0; l < letters.length; l++) {
-                for (int s = 0; s < letters.length; s++) {
+        for (int i = 0; i < LETTERS.length; i++) {
+            for (int l = 0; l < LETTERS.length; l++) {
+                for (int s = 0; s < LETTERS.length; s++) {
                     for (int j = 0; j < 10; j++) {
                         for (int k = 1; k <= 199; k++) {
-                            String letter1 = letters[i];
-                            String letter2 = letters[l];
-                            String letter3 = letters[s];
+                            String letter1 = LETTERS[i];
+                            String letter2 = LETTERS[l];
+                            String letter3 = LETTERS[s];
                             String region = String.valueOf(k);
                             if (k < 10) {
                                 region = "0" + region;
@@ -36,18 +31,12 @@ public class CoolNumbers_Option_2 {
         return listNumber;
     }
 
-    public static String timeWork(long time) {
-        return ANSWER_SYSTEM_TIME + (System.nanoTime() - time) + " " + "нс";
-    }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        long startTime1 = System.nanoTime();
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).equals(number)) {
-                System.out.println(ANSWER_SYSTEM_SEARCH_ARRAY_LIST + ANSWER_SYSTEM + timeWork(startTime1));
                 return true;
             } else {
-                System.out.println(ANSWER_SYSTEM_SEARCH_ARRAY_LIST + ANSWER_SYSTEM_ERROR + timeWork(startTime1));
             }
         }
         return false;
@@ -56,38 +45,18 @@ public class CoolNumbers_Option_2 {
 
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        long startTime2 = System.nanoTime();
-        if (Collections.binarySearch(sortedList, number) >= 0) {
-            System.out.println(ANSWER_SYSTEM_SEARCH_BINARY + ANSWER_SYSTEM + timeWork(startTime2));
-            return true;
-        } else {
-            System.out.println(ANSWER_SYSTEM_SEARCH_BINARY + ANSWER_SYSTEM_ERROR + timeWork(startTime2));
-        }
-        return false;
+        return Collections.binarySearch(sortedList, number) >= 0;
     }
 
 
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        long startTime3 = System.nanoTime();
-        if (hashSet.contains(number)) {
-            System.out.println(ANSWER_SYSTEM_SEARCH_HASH_SET + ANSWER_SYSTEM + timeWork(startTime3));
-            return true;
-        } else {
-            System.out.println(ANSWER_SYSTEM_SEARCH_HASH_SET + ANSWER_SYSTEM_ERROR + timeWork(startTime3));
-        }
-        return false;
+        return hashSet.contains(number);
     }
 
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        long startTime3 = System.nanoTime();
-        if (treeSet.contains(number)) {
-            System.out.println(ANSWER_SYSTEM_SEARCH_TREE_SET + ANSWER_SYSTEM + timeWork(startTime3));
-            return true;
-        } else {
-            System.out.println(ANSWER_SYSTEM_SEARCH_TREE_SET + ANSWER_SYSTEM_ERROR + timeWork(startTime3));
-        }
-        return false;
+        return (treeSet.contains(number));
     }
+
 }
 
--- src/Module_10/TestCoolNumbers_Option_2.java
+++ src/Module_10/TestCoolNumbers_Option_2.java
@@ -6,6 +6,13 @@ public class TestCoolNumbers_Option_2 {
 
     private static Scanner scanner = new Scanner(System.in);
     private static final String MESSAGE_USER_INPUT_AUTO_NUMBER = "Введите ГосНомер автомобиля: ";
+    private static final String ANSWER_SYSTEM_TIME = "Поиск занял: ";
+    private static final String ANSWER_SYSTEM_SEARCH_ARRAY_LIST = "Поиск перебором: ";
+    private static final String ANSWER_SYSTEM_ERROR = "Номер НЕ найден: ";
+    private static final String ANSWER_SYSTEM = "Номер найден! ";
+    private static final String ANSWER_SYSTEM_SEARCH_BINARY = "Бинарный поиск : ";
+    private static final String ANSWER_SYSTEM_SEARCH_HASH_SET = "Поиск в HashSet: ";
+    private static final String ANSWER_SYSTEM_SEARCH_TREE_SET = "Поиск в TreeSet: ";
 
     public static void main(String[] args) {
 
@@ -14,18 +21,43 @@ public class TestCoolNumbers_Option_2 {
             List<String> listGenerated = CoolNumbers_Option_2.generateCoolNumbers();
             String inputAutoNumberUser = scanner.nextLine();
 
-            CoolNumbers_Option_2.bruteForceSearchInList(listGenerated, inputAutoNumberUser);
+            long startTime1 = System.nanoTime();
+            if (!CoolNumbers_Option_2.bruteForceSearchInList(listGenerated, inputAutoNumberUser)) {
+                ptint(ANSWER_SYSTEM_SEARCH_ARRAY_LIST, ANSWER_SYSTEM_ERROR, startTime1);
+            } else {
+                ptint(ANSWER_SYSTEM_SEARCH_ARRAY_LIST, ANSWER_SYSTEM, startTime1);
+            }
+
             Collections.sort(listGenerated);
-            CoolNumbers_Option_2.binarySearchInList(listGenerated, inputAutoNumberUser);
+            long startTime2 = System.nanoTime();
+            if (!CoolNumbers_Option_2.binarySearchInList(listGenerated, inputAutoNumberUser)) {
+                TestCoolNumbers_Option_2.ptint(ANSWER_SYSTEM_SEARCH_BINARY, ANSWER_SYSTEM_ERROR, startTime2);
+            } else {
+                ptint(ANSWER_SYSTEM_SEARCH_BINARY, ANSWER_SYSTEM, startTime2);
+            }
 
             HashSet<String> listHashSet = new HashSet<>(listGenerated);
-            CoolNumbers_Option_2.searchInHashSet(listHashSet, inputAutoNumberUser);
+            long startTime3 = System.nanoTime();
+            if (!CoolNumbers_Option_2.searchInHashSet(listHashSet, inputAutoNumberUser)) {
+                ptint(ANSWER_SYSTEM_SEARCH_HASH_SET, ANSWER_SYSTEM_ERROR, startTime3);
+            } else {
+                ptint(ANSWER_SYSTEM_SEARCH_BINARY, ANSWER_SYSTEM, startTime3);
+            }
 
             TreeSet<String> listTreeSet = new TreeSet<>(listGenerated);
-            CoolNumbers_Option_2.searchInTreeSet(listTreeSet, inputAutoNumberUser);
+            long startTime4 = System.nanoTime();
+            if (!CoolNumbers_Option_2.searchInTreeSet(listTreeSet, inputAutoNumberUser)) {
+                ptint(ANSWER_SYSTEM_SEARCH_TREE_SET, ANSWER_SYSTEM_ERROR, startTime4);
+            } else {
+                ptint(ANSWER_SYSTEM_SEARCH_TREE_SET, ANSWER_SYSTEM, startTime4);
+            }
 
         }
 
+    }
+
+    public static void ptint(String TYPE_ANSWER, String answerTime, long time) {
+        System.out.println(TYPE_ANSWER + answerTime + (System.nanoTime() - time) + " " + "нс");
     }
 }
 
